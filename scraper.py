5da565af6a1543422bbd54eb64fa5c89314f0ad9
David Lemayian <dlemayian@gmail.com>
2017-03-14 11:37:53 +0300

    And we've added the tags pulled from Watson.

--- scraper.py
+++ scraper.py
@@ -2,12 +2,36 @@ import requests
 import boto3
 import os, json
 
+import sys
+
+import watson_developer_cloud
+import watson_developer_cloud.natural_language_understanding.features.v1 as features
+
+nlu = watson_developer_cloud.NaturalLanguageUnderstandingV1(
+    version='2017-02-27',
+    username=os.environ['MORPH_WATSON_USERNAME'],
+    password=os.environ['MORPH_WATSON_PASSWORD'])
+
 r = requests.get('http://www.the-star.co.ke/api/mobile/views/mobile_app?args[0]=24&limit=50')
 
 feed = r.json()
 articles = []
 
 for article in feed:
+    tags = []
+    tags_weighted = []
+
+    response = nlu.analyze(
+        text=article['body'],
+        features=[features.Concepts()])
+
+    for concept in response['concepts'][:5]:
+        tags.append(concept['text'])
+        tags_weighted.append({concept['text']: concept['relevance']})
+
+    article['sorted_tags'] = list(set(tags))
+    article['theme'] = tags_weighted
+
     articles.append({'node': article})
 
 data = {
